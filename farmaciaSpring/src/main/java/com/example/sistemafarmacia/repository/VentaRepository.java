b5660d36f01edc03d7f8977c439ff174eccc0956
Andersson Jaren Flores Ruiz <andersonfloresruiz@gmail.com>
2025-07-25 17:32:25 -0500

    v

--- farmaciaSpring/src/main/java/com/example/sistemafarmacia/repository/VentaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.sistemafarmacia.repository;
-
-import com.example.sistemafarmacia.model.Venta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VentaRepository extends JpaRepository<Venta, Integer> {
-}
